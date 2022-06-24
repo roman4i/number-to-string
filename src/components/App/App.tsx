8e94f00a784e0eabf58491bcbbbf918b495de9c9
roman4i <verb.r17@gmail.com>
2022-06-24 13:57:57 +0300

    Added container for the app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import Box from '@mui/material/Box';
 
 function App() {
+  const MainBoxStyle = {
+    width: 600,
+    marginLeft: 'auto', 
+    marginRight: 'auto', 
+    marginTop: 5,
+    border: '1px lightgray solid', 
+    borderRadius: 3 ,
+    padding: 3,
+  }
+
   return (
-    <div>Something</div>
+    <Box sx={ MainBoxStyle }>
+
+    </Box>
   );
 }
 
